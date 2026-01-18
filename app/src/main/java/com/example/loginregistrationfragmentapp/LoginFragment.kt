282d40744ddb58875daaf8dc0f8c4c612650b3b9
shukurapp <125639931+shukurapp@users.noreply.github.com>
2026-01-18 19:05:31 +0400

    Update LoginFragment.kt

--- app/src/main/java/com/example/loginregistrationfragmentapp/LoginFragment.kt
+++ app/src/main/java/com/example/loginregistrationfragmentapp/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginFragment : Fragment() {
 
@@ -12,7 +15,25 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val emailEditText = view.findViewById<EditText>(R.id.email)
+        val passwordEditText = view.findViewById<EditText>(R.id.password)
+        val loginButton = view.findViewById<Button>(R.id.btn)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            Toast.makeText(
+                requireContext(),
+                "Sizin login $email-di, şifrəniz $password-di",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
